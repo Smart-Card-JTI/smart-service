1041821fc167df9702270e2c2f6d39d92fba31b2
yafiabiyyu <luckyveri3@gmail.com>
2020-03-07 23:09:32 +0700

    salah ketik

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -6,6 +6,6 @@ from flask_restplus import Api, Resource, fields
 from .kantung_parkir_controller import api as kantung_parkir_ns
 
 controller = Blueprint('api',__name__,url_prefix='/api/v1')
-api = Api(controller, version='1.0', title='SmartCard API', description='API SmartCard')
+api = Api(controller, version='1.0', title='Smart-Parking API', description='API Smart-Parking')
 
 api.add_namespace(kantung_parkir_ns,path='/parkir')
