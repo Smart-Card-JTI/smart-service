be5d8eb25baf21540f5551b06b692e93abd2e772
yafiabiyyu <luckyveri3@gmail.com>
2020-03-18 08:46:52 +0700

    disable sementara service kantung parkir

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -4,10 +4,10 @@ from flask_restplus import Api, Resource, fields
 
 #import namsespace
 from .kantung_parkir_controller import api as kantung_parkir_ns
-from .data_persolan_controller import api as data_personal_ns
+from .data_kartu_controller import api as data_kartu
 
-controller = Blueprint('api',__name__,url_prefix='/api/v1')
+controller = Blueprint('api',__name__)
 api = Api(controller, version='1.0', title='Smart-Parking API', description='API Smart-Parking')
 
-api.add_namespace(kantung_parkir_ns,path='/parkir')
-api.add_namespace(data_personal_ns,path='/data')
+# api.add_namespace(kantung_parkir_ns,path='/data/parkir')
+api.add_namespace(data_kartu,path='/data/kartu')
