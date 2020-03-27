a2af125577f7c7fafc5d3bedbe80f2258698b7e4
yafiabiyyu <luckyveri3@gmail.com>
2020-03-27 16:15:45 +0700

    check url prefix

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -7,7 +7,7 @@ from .kantung_parkir_controller import api as kantung_parkir_ns
 from .data_kartu_controller import api as data_kartu
 from .mahasiswa_controler import api as data_mahasiswa
 controller = Blueprint('api',__name__)
-api = Api(controller, version='1.0', title='Smart-Parking API', description='API Smart-Parking')
+api = Api(controller, version='1.0', title='Smart-Parking API',url_prefix='/api/v1', description='API Smart-Parking')
 
 api.add_namespace(kantung_parkir_ns,path='/data/parkir')
 api.add_namespace(data_kartu,path='/data/kartu')
